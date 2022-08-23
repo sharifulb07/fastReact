faab2d06437ef5144651c8f6be40587a502ca6f6
Shariful Islam <98572737+sharifulb07@users.noreply.github.com>
2022-08-23 16:21:31 +0600

    Hello Intro linking into different state here

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 
+import JSX from "./jsx";
 
 export default function App() {
   return (
@@ -11,25 +12,21 @@ export default function App() {
   );
 }
 
-
-class Test extends React.Component{
-  constructor(props){
+class Test extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      age:34
-    }
+    this.state = {
+      age: 34
+    };
   }
-    render(){
-      return(
-        <>
-      <h1>Welcome to {this.props.name} world </h1>
-
-      <p>my age is {this.state.age} years old </p>
-
-        </>
-      )
-    }
-  
-
-
+  render() {
+    return (
+      <>
+        <h1> My journey sets in as frontend developer with react </h1>;
+        <h1>Welcome to {this.props.name} world </h1>
+        <p>my age is {this.state.age} years old </p>
+        <JSX />
+      </>
+    );
+  }
 }
---
+++ src/jsx.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const name = "This is Ashia Intro";
+
+export default function testJsx() {
+  return (
+    <>
+      <h1> description: {name} </h1>,<p>done </p>
+    </>
+  );
+}
